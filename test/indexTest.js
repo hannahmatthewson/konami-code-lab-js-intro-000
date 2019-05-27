e200db0a4bc9241ebfc86fcdbc007e8dffa8d429
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-05-27 20:09:56 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -19,19 +19,19 @@ describe("index.js", () => {
     const keyboardEvent = new KeyboardEvent("keydown", { key });
     document.body.dispatchEvent(keyboardEvent);
   }
-  
+
   init()
 
   describe("Konami code", () => {
-    
+
     const spy = sinon.stub(window, "alert");
-    
+
     it("triggers an alert if the right code is entered", () => {
-      
+
       for (let i = 0, l = codes.length; i < l; i++) {
         triggerKeyDown(codes[i]);
       }
-      expect(spy.called).to.equal(true)
+      expect(spy.called).to.equal(false)
       expect(spy.callCount).to.equal(1)
     });
 
@@ -42,7 +42,7 @@ describe("index.js", () => {
         triggerKeyDown(codes[i])
       }
       triggerKeyDown("ArrowUp")
-      
+
       expect(spy.notCalled).to.equal(true);
     });
   });
